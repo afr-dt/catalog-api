1beb7b3bbba7eb1b1b360de244a7999df0ae9e54
aleks <aleksgnu@gmail.com>
2018-09-03 00:22:35 -0500

    Connect with PostgreSQL and create Areas and Catalogs

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('sqlite:///api.sqlite3', convert_unicode=True)
+engine = create_engine(
+    'postgresql://afr:d3VDJ4n60OneD4yIwalk4lon3@localhost/graphqltask', convert_unicode=True)
 db_session = scoped_session(sessionmaker(
     autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
--- mutations/mutations.py
+++ mutations/mutations.py
@@ -36,3 +36,57 @@ class CreateItem(graphene.Mutation):
         except BaseException as err:
             db_session.rollback()
             raise GraphQLError("Error inserting Item... {}".format(err))
+
+
+class AreaInput(graphene.InputObjectType):
+    """Area Input"""
+    area_id = graphene.Int(required=False)
+    name = graphene.String(required=False)
+    description = graphene.String(required=False)
+    created = graphene.String(required=False)
+    updated = graphene.String(required=False)
+
+
+class CreateArea(graphene.Mutation):
+    """Create Area"""
+    class Arguments:
+        area_data = AreaInput(required=True)
+    area = graphene.Field(Area)
+    status = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, area_data=None):
+        try:
+            area = AreaModel(**area_data)
+            db_session.add(area)
+            db_session.commit()
+        except BaseException as err:
+            db_session.rollback()
+            raise GraphQLError("Error inserting Area... {}".format(err))
+
+
+class CatalogInput(graphene.InputObjectType):
+    """Catalog Input"""
+    catalog_id = graphene.Int(required=False)
+    name = graphene.String(required=False)
+    description = graphene.String(required=False)
+    created = graphene.String(required=False)
+    updated = graphene.String(required=False)
+
+
+class CreateCatalog(graphene.Mutation):
+    """Create Catalog"""
+    class Arguments:
+        catalog_data = CatalogInput(required=True)
+    catalog = graphene.Field(Catalog)
+    status = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, catalog_data=None):
+        try:
+            catalog = CatalogModel(**catalog_data)
+            db_session.add(catalog)
+            db_session.commit()
+        except BaseException as err:
+            db_session.rollback()
+            raise GraphQLError("Error inserting Catalog... {}".format(err))
